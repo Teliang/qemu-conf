504f6a97eb966c591931341ccbe535734432641a
teliang <telianghung@outlook.com>
2026-06-28 16:16:53 +0900

    arch-vm

--- arch-vm/bootup.sh
+++ arch-vm/bootup.sh
@@ -1,1 +1,1 @@
-qemu-system-x86_64 -m 2048 -cpu host -enable-kvm -drive file=your_image.qcow2,format=qcow2 -nic user,hostfwd=tcp::22220-:22 -nographic -smp cores=8,threads=2,sockets=1,maxcpus=16
+qemu-system-x86_64 -m 2048 -cpu host -enable-kvm -drive file=your_image.qcow2,format=qcow2 -nic user,hostfwd=tcp::22220-:22 -nographic -smp cpus=8,cores=4,threads=2,sockets=1
